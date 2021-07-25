8967067561b843e902ad54eeb4f0dce263adb432
Eric Azevedo <info@ericazevedo.com>
2021-07-25 18:35:06 -0300

    Retirada de comentários

--- src/main/java/com/ericazevedo/bookstoremanager/BookstoremanagerApplication.java
+++ src/main/java/com/ericazevedo/bookstoremanager/BookstoremanagerApplication.java
@@ -3,10 +3,6 @@ package com.ericazevedo.bookstoremanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * @author Eric Rodrigues
- *
- */
 @SpringBootApplication
 public class BookstoremanagerApplication {
 
